b2b99498be4fae49782cd3041e66381a4d75eaab
Maciej Bilas <maciej.bilas@gmail.com>
2014-05-29 21:36:18 +0200

    Improved EmbeddedJetty.

--- garden-web/src/main/scala/com/softwaremill/thegarden/web/jetty/EmbeddedJetty.scala
+++ garden-web/src/main/scala/com/softwaremill/thegarden/web/jetty/EmbeddedJetty.scala
@@ -4,29 +4,34 @@ import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.webapp.WebAppContext
 import java.net.InetSocketAddress
 
-trait EmbeddedJetty {
-  protected var jetty: Server = null
+class EmbeddedJetty(config: WebServerConfig, contextAttributes: Map[String, AnyRef] = Map()) {
+
+  lazy val jetty = {
+    val server = new Server(new InetSocketAddress(config.webServerHost, config.webServerPort))
+    server.setHandler(prepareContext())
+    server
+  }
 
   def startJetty() {
-    jetty = new Server(jettyAddress)
-    jetty.setHandler(prepareContext())
     jetty.start()
   }
 
   protected def prepareContext() = {
-    val context = new WebAppContext()
-    context.setContextPath("/")
-    setResourceBase(context)
-    context
+    val webContext = new WebAppContext()
+    webContext.setContextPath("/")
+    val webappDirInsideJar = webContext.getClass.getClassLoader.getResource("webapp").toExternalForm
+    webContext.setWar(webappDirInsideJar)
+
+    contextAttributes.foreach {
+      case (k, v) =>
+        webContext.setAttribute(k, v)
+    }
+
+    webContext
   }
 
   def stopJetty() {
     jetty.stop()
   }
 
-  protected def setResourceBase(context: WebAppContext)
-
-  def webServerConfig: WebServerConfig
-
-  lazy val jettyAddress = new InetSocketAddress(webServerConfig.webServerHost, webServerConfig.webServerPort)
 }
