2fadc8a6ca5c6e8e98aa8ec6d04f05cb97145034
Maciej Bilas <maciej.bilas@gmail.com>
2014-09-01 15:30:25 +0200

    Bump javac versions.

--- project/Build.scala
+++ project/Build.scala
@@ -83,7 +83,7 @@ object TheGardenBuild extends Build {
     scalacOptions in GlobalScope in Compile := Seq("-unchecked", "-deprecation", "-feature"),
     scalacOptions in GlobalScope in Test := Seq("-unchecked", "-deprecation", "-feature"),
     // http://stackoverflow.com/questions/21435023/how-to-change-jdk-set-by-sbt-import-in-intellij-idea
-    javacOptions in Compile ++= Seq("-source", "1.7", "-target", "1.7"),
+    javacOptions in Compile ++= Seq("-source", "1.8", "-target", "1.8"),
     scalaVersion := "2.11.2",
     crossScalaVersions := Seq("2.10.2", "2.11.2"),
     organization := "com.softwaremill.thegarden",
