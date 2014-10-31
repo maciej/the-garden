19991a8e3e9aa8c9928cd51362aecce3d5db298e
Maciej Bilas <maciej.bilas@gmail.com>
2014-10-31 13:02:31 +0100

    Add TimeoutExtension.withImplicitTimeout.

--- garden-akka/src/main/scala/com/softwaremill/thegarden/akka/TimeoutExtension.scala
+++ garden-akka/src/main/scala/com/softwaremill/thegarden/akka/TimeoutExtension.scala
@@ -3,6 +3,7 @@ package com.softwaremill.thegarden.akka
 import java.util.concurrent.TimeoutException
 
 import akka.actor.ActorSystem
+import akka.util.Timeout
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration.FiniteDuration
@@ -10,9 +11,13 @@ import scala.concurrent.duration.FiniteDuration
 
 object TimeoutExtension {
 
+  def withImplicitTimeout[T](future: Future[T])
+                            (implicit system : ActorSystem, ec: ExecutionContext, timeout : Timeout) : Future[T] =
+    withTimeout(timeout.duration)(future)
+
   // Taken from http://semberal.github.io/scala-future-timeout-patterns.html
-  def withTimeout[T](timeout: FiniteDuration)(future: Future[T])(implicit actorSystem: ActorSystem,
-                                                                 ec: ExecutionContext): Future[T] = {
+  def withTimeout[T](timeout: FiniteDuration)(future: Future[T])
+                    (implicit actorSystem: ActorSystem, ec: ExecutionContext): Future[T] = {
 
     val timeoutFuture = akka.pattern.after(timeout, using = actorSystem.scheduler) {
       Future.failed(new TimeoutException(s"The future did not complete in $timeout."))
